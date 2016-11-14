53c3025f68a6aa681d253ebd4f1381d7d8011391
xkuzz <davidcr96@gmail.com>
2016-11-14 13:55:29 +0100

    GUI del Cliente

---
+++ src/cliente/Cliente.java
@@ -0,0 +1,20 @@
+package cliente;
+
+/**
+ *
+ * @author Alejandro Campoy Nieves.
+ * @author David Criado Ramón.
+ * 
+ * La clase Cliente es la clase principal y se encarga de inicializar la interfaz
+ * gráficar
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/cliente/ClienteTCP.java
@@ -0,0 +1,10 @@
+package cliente;
+
+/**
+ *
+ * @author Alejandro Campoy Nieves.
+ * @author David Criado Ramón.
+ */
+public class ClienteTCP {
+    
+}
